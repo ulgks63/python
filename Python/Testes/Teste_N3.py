99e5c5ce30b42993fbe045b19e28f448c669d339
Ulgks <104450799+ulgks63@users.noreply.github.com>
2022-08-17 08:56:04 -0300

    Create Teste_N3.py
    Um pequeno e bem simples chat de conversa com respostas limitadas

---
+++ Python/Testes/Teste_N3.py
@@ -0,0 +1,29 @@
+print("Olá")
+r=input("Responde o que? ")
+if r=="Oi":
+    print("Tudo bem? ")
+if r=="oi":
+    print("Tudo bem?")
+if r==("modo de deputação"):
+    print("Entrando, aguarde")
+    print("Menu de deputação ativo!")
+    print("1-Fechar a Aplicação; 2-Entrar no log; 3-Sair do modo deputação")
+    d=input("Aguandando comando ")
+    if d=="1": exit(1)
+    if d=="2":print("Log de 09 de maio de 2022; Tudo ocorreu bem, a humanidade foi extinda com sucesso!"), exit(1)
+    if d=="3":print("Saindo!"),print("ALERTA MODO DE DEPUTAÇÃO ESTA DESATIVADO ATÉ SEGUNDAS ORDENS")
+
+print("Olá")
+r2=input("Responder o que? ")
+if r2=="Oi":
+    print("Tudo bem? ")
+if r2=="oi":
+    print("Tudo bem?")
+t=input("Responder o que? ")
+if t=="sim":
+    print("Que bom! :D")
+if t=="não":
+    print("Nossa que pena amigo, precisa de ajuda em alguma coisa? ")
+
+
+print("SIMULAÇÃO Nº3462: OLHE O LOG!!!!!")
